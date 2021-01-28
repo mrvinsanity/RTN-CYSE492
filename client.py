803499297d22b6604958e91e65ec34fb043d27f7
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-01-28 10:57:17 -0500

    Update client.py

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ while True:
     t = 10
     queries = queryDB(t)
     if queries != []:
+        print(queries)
         uploadFile(queries)
     else:
         print("No queries in the last %s seconds" % (t))
