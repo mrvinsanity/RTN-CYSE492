e57370171cbc41d96ea23f66cfaace3e6ac833aa
Ajith Sumesh <asumesh15@users.noreply.github.com>
2021-01-28 10:34:41 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -2,7 +2,10 @@ from upload import *
 import time
 
 while True:
-    queries = queryDB(10)
+    t = 10
+    queries = queryDB(t)
     if queries != []:
         uploadFile(queries)
+    else:
+        print("No queries in the last %s seconds" % (t))
     time.sleep(10)
